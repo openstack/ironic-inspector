7b6d4f8cf7ca26420e967ae0484e2dcc2c7d58fc
Dmitry Tantsur <dtantsur@redhat.com>
2014-10-03 13:16:43 +0200

    Added main script and updated setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,11 @@ from setuptools import setup
 setup(
     name = "ironic-discoverd",
     version = "0.0.1",
+    description = "Simple hardware discovery for OpenStack Ironic",
     author = "Dmitry Tantsur",
     author_email = "dtansur@redhat.com",
+    url = "https://github.com/Divius/ironic-discoverd/",
     packages=['ironic_discoverd'],
     install_requires = ['Flask', 'python-ironicclient'],
+    scripts = ['bin/ironic-discoverd'],
 )
