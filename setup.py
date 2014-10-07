b3ea94c17b366ba966905c501c6486c67cad9902
Dmitry Tantsur <dtantsur@redhat.com>
2014-10-07 12:12:28 +0200

    Version 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = "ironic-discoverd",
-    version = "0.1",
+    version = "0.1.0",
     description = "Simple hardware discovery for OpenStack Ironic",
     author = "Dmitry Tantsur",
     author_email = "dtansur@redhat.com",
