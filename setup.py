1f5e21bb14ec6cb6cf1c1516125a512b9c42f20a
Dmitry Tantsur <dtantsur@redhat.com>
2014-10-14 10:56:28 +0200

    Updated description in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name = "ironic-discoverd",
     version = "0.2.0",
-    description = "Simple hardware discovery for OpenStack Ironic",
+    description = "Hardware properties discovery for OpenStack Ironic",
     author = "Dmitry Tantsur",
     author_email = "dtansur@redhat.com",
     url = "https://github.com/Divius/ironic-discoverd/",
