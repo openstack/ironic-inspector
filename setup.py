9dcc2bb082fb568b978db975e2e7b5d0a48e7e4b
Dmitry Tantsur <dtantsur@redhat.com>
2015-06-18 16:08:07 +0200

    Switch to pbr
    Also creates a required "venv" environment, drops version handling
    from __init__.py and updates requirements.
    
    Closes-Bug: #1466477
    Change-Id: I57ce71ee52cc48e0239cdecb69fd2272fc99ea41

--- setup.py
+++ setup.py
@@ -1,68 +1,30 @@
-import re
+#!/usr/bin/env python
+# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-from setuptools import setup
+# THIS FILE IS MANAGED BY THE GLOBAL REQUIREMENTS REPO - DO NOT EDIT
+import setuptools
 
-
+# In python < 2.7.4, a lazy loading of package `pbr` will break
+# setuptools if some other modules registered functions in `atexit`.
+# solution from: http://bugs.python.org/issue15881#msg170215
 try:
-    # Distributions have to delete *requirements.txt
-    with open('requirements.txt', 'r') as fp:
-        install_requires = [re.split(r'[<>=~]', line)[0]
-                            for line in fp if line.strip()]
-except EnvironmentError:
-    print("No requirements.txt, not handling dependencies")
-    install_requires = []
+    import multiprocessing  # noqa
+except ImportError:
+    pass
 
-
-with open('ironic_inspector/__init__.py', 'rb') as fp:
-    exec(fp.read())
-
-
-setup(
-    name = "ironic-inspector",
-    version = __version__,
-    description = open('README.rst', 'r').readline().strip(),
-    author = "Dmitry Tantsur",
-    author_email = "dtantsur@redhat.com",
-    url = "https://pypi.python.org/pypi/ironic-inspector",
-    packages = ['ironic_inspector', 'ironic_inspector.plugins',
-                'ironic_inspector.test', 'ironic_inspector.common',
-                'ironic_inspector_ramdisk', 'ironic_inspector_ramdisk.test'],
-    install_requires = install_requires,
-    # because entry points don't work with multiple packages
-    scripts = ['bin/ironic-inspector-ramdisk'],
-    entry_points = {
-        'console_scripts': [
-            "ironic-inspector = ironic_inspector.main:main",
-        ],
-        'ironic_inspector.hooks.processing': [
-            "scheduler = ironic_inspector.plugins.standard:SchedulerHook",
-            "validate_interfaces = ironic_inspector.plugins.standard:ValidateInterfacesHook",
-            "ramdisk_error = ironic_inspector.plugins.standard:RamdiskErrorHook",
-            "example = ironic_inspector.plugins.example:ExampleProcessingHook",
-            "extra_hardware = ironic_inspector.plugins.extra_hardware:ExtraHardwareHook",
-            "root_device_hint = ironic_inspector.plugins.root_device_hint:RootDeviceHintHook",
-        ],
-        'ironic_inspector.hooks.node_not_found': [
-            "example = ironic_inspector.plugins.example:example_not_found_hook",
-        ],
-        'openstack.cli.extension': [
-            'baremetal-introspection = ironic_inspector.shell',
-        ],
-        'openstack.baremetal_introspection.v1': [
-            "baremetal_introspection_start = ironic_inspector.shell:StartCommand",
-            "baremetal_introspection_status = ironic_inspector.shell:StatusCommand",
-        ],
-        'oslo.config.opts': [
-            "ironic_inspector = ironic_inspector.conf:list_opts",
-            "ironic_inspector.common.swift = ironic_inspector.common.swift:list_opts"
-        ],
-    },
-    classifiers = [
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: OpenStack',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: POSIX',
-    ],
-    license = 'APL 2.0',
-)
+setuptools.setup(
+    setup_requires=['pbr'],
+    pbr=True)
