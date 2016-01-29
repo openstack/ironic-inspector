b28f1a75b7ad3ff1b79ead66f3a12e2368d16dc5
Andreas Jaeger <aj@suse.com>
2016-01-29 09:34:46 +0100

    Update translation setup
    Follow new infra setup for translations, see spec
    http://specs.openstack.org/openstack-infra/infra-specs/specs/translation_setup.html
    for full details.
    
    This basically renames
    ironic-inspector/locale/ironic-inspector.pot to
    ironic_inspector/locale/ironic_inspector.pot. For this we need to update
    setup.cfg. Also fix some errors for translation setup in setup.cfg
    
    Update also domain name in i18n.py.
    
    The project has no translations currently, let's remove the outdated
    pot file, the updated scripts work without them. So, we can just
    delete the file and once there are translations, an updated pot file
    together with translations can be imported automatically.
    
    Change-Id: I1f9e3777a68d09406eeee836d03f15cbfe568470

--- ironic_inspector/common/i18n.py
+++ ironic_inspector/common/i18n.py
@@ -15,7 +15,7 @@
 
 import oslo_i18n
 
-_translators = oslo_i18n.TranslatorFactory(domain='ironic-inspector')
+_translators = oslo_i18n.TranslatorFactory(domain='ironic_inspector')
 
 # The primary translation function using the well-known name "_"
 _ = _translators.primary
