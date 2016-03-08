5bb2f535892f34349fcb40870a49e0628a37d3de
Dmitry Tantsur <dtantsur@redhat.com>
2016-03-08 17:46:13 +0100

    [devstack] add support for using iPXE instead of plain PXE
    Makes our devstack plugin respect the IRONIC_IPXE_ENABLED variable.
    
    Partial-Bug: #1416371
    Change-Id: I16c6454ba5e1339acfe302baa68e69b9a81f9a18
    (cherry picked from commit be14aae263a8b6bb46796f10bb9461d20629a667)

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -125,11 +125,25 @@ function prepare_tftp {
         fi
     fi
 
-    mkdir_chown_stack "$IRONIC_TFTPBOOT_DIR/pxelinux.cfg"
-    cp $IRONIC_INSPECTOR_KERNEL_PATH $IRONIC_TFTPBOOT_DIR/ironic-inspector.kernel
-    cp $IRONIC_INSPECTOR_INITRAMFS_PATH $IRONIC_TFTPBOOT_DIR
+    if [[ "$IRONIC_IPXE_ENABLED" == "True" ]] ; then
+        cp $IRONIC_INSPECTOR_KERNEL_PATH $IRONIC_HTTP_DIR/ironic-inspector.kernel
+        cp $IRONIC_INSPECTOR_INITRAMFS_PATH $IRONIC_HTTP_DIR
 
-    cat > "$IRONIC_TFTPBOOT_DIR/pxelinux.cfg/default" <<EOF
+        cat > "$IRONIC_HTTP_DIR/ironic-inspector.ipxe" <<EOF
+#!ipxe
+
+dhcp
+
+kernel http://$IRONIC_HTTP_SERVER:$IRONIC_HTTP_PORT/ironic-inspector.kernel BOOTIF=\${mac} $IRONIC_INSPECTOR_KERNEL_CMDLINE
+initrd http://$IRONIC_HTTP_SERVER:$IRONIC_HTTP_PORT/ironic-inspector.initramfs
+boot
+EOF
+    else
+        mkdir_chown_stack "$IRONIC_TFTPBOOT_DIR/pxelinux.cfg"
+        cp $IRONIC_INSPECTOR_KERNEL_PATH $IRONIC_TFTPBOOT_DIR/ironic-inspector.kernel
+        cp $IRONIC_INSPECTOR_INITRAMFS_PATH $IRONIC_TFTPBOOT_DIR
+
+        cat > "$IRONIC_TFTPBOOT_DIR/pxelinux.cfg/default" <<EOF
 default inspect
 
 label inspect
@@ -138,6 +152,7 @@ append initrd=ironic-inspector.initramfs $IRONIC_INSPECTOR_KERNEL_CMDLINE
 
 ipappend 3
 EOF
+    fi
 }
 
 function configure_inspector {
@@ -201,7 +216,20 @@ function configure_inspector_swift {
 function configure_inspector_dhcp {
     mkdir_chown_stack "$IRONIC_INSPECTOR_CONF_DIR"
 
-    cat > "$IRONIC_INSPECTOR_DHCP_CONF_FILE" <<EOF
+    if [[ "$IRONIC_IPXE_ENABLED" == "True" ]] ; then
+        cat > "$IRONIC_INSPECTOR_DHCP_CONF_FILE" <<EOF
+no-daemon
+port=0
+interface=$IRONIC_INSPECTOR_INTERFACE
+bind-interfaces
+dhcp-range=$IRONIC_INSPECTOR_DHCP_RANGE
+dhcp-match=ipxe,175
+dhcp-boot=tag:!ipxe,undionly.kpxe
+dhcp-boot=tag:ipxe,http://$IRONIC_HTTP_SERVER:$IRONIC_HTTP_PORT/ironic-inspector.ipxe
+dhcp-sequential-ip
+EOF
+    else
+        cat > "$IRONIC_INSPECTOR_DHCP_CONF_FILE" <<EOF
 no-daemon
 port=0
 interface=$IRONIC_INSPECTOR_INTERFACE
@@ -209,6 +237,7 @@ bind-interfaces
 dhcp-range=$IRONIC_INSPECTOR_DHCP_RANGE
 dhcp-boot=pxelinux.0
 EOF
+    fi
 }
 
 function prepare_environment {
@@ -229,6 +258,9 @@ function prepare_environment {
 function cleanup_inspector {
     rm -f $IRONIC_TFTPBOOT_DIR/pxelinux.cfg/default
     rm -f $IRONIC_TFTPBOOT_DIR/ironic-inspector.*
+    if [[ "$IRONIC_IPXE_ENABLED" == "True" ]] ; then
+        rm -f $IRONIC_HTTP_DIR/ironic-inspector.*
+    fi
     sudo rm -f /etc/sudoers.d/ironic-inspector-rootwrap
 
     # Try to clean up firewall rules
