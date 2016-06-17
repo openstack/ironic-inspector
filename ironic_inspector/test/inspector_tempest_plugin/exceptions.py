fb6bcbec9ce5380f5da17ad58a9b35255ca40dbd
Anton Arefiev <aarefiev@mirantis.com>
2016-06-17 11:50:40 +0300

    Fix tempest tests
    Base TempestException was removed  from tempest, now exceptions based on
    tempest.lib TempestException, inherited from restclient exceptions.
    So inherit inspector tempest exceptions from last one.
    
    Change-Id: I8058a964e837dbb4aa5a8b214f216453a18a1713

--- ironic_inspector/test/inspector_tempest_plugin/exceptions.py
+++ ironic_inspector/test/inspector_tempest_plugin/exceptions.py
@@ -10,7 +10,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from tempest import exceptions
+from tempest.lib import exceptions
 
 
 class IntrospectionFailed(exceptions.TempestException):
