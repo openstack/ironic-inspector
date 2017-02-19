d9e7a459a015a8b9704be59715893c72018e5dd6
dparalen <mkovacik@redhat.com>
2017-02-19 14:14:21 +0100

    [Devstack]: pick correct MTU for br-inspector veth
    This patch pick correct MTU for veth pair created by inspector to avoid
    errors when packets are dropped.
    
    The following commit is squashed into it to make the cherry-pick clean:
    
    Use IRONIC_VM_NETWORK_BRIDGE
    
    devstack/plugin.sh hardwires brbm as the VM bridge.
    Referencing ironic plugin VM variable makes more sense.
    
    Change-Id: Ie1036ca75c9561393f4efc2478d0a2c9cba9e089
    Closes-Bug: 1641019
    (cherry picked from commit 328acd4d0a74e2ecda4f86db2b4f0716639835f3)
    (cherry picked from commit ae882c5b02e0ea7a47f8ca808c79a941fad39ebf)

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -256,9 +256,11 @@ function prepare_environment {
     if [[ "$IRONIC_IS_HARDWARE" == "False" ]]; then
         sudo ip link add brbm-inspector type veth peer name $IRONIC_INSPECTOR_INTERFACE
         sudo ip link set dev brbm-inspector up
-        sudo ovs-vsctl add-port brbm brbm-inspector
+        sudo ovs-vsctl add-port $IRONIC_VM_NETWORK_BRIDGE brbm-inspector
     fi
     sudo ip link set dev $IRONIC_INSPECTOR_INTERFACE up
+    sudo ip link set dev brbm-inspector mtu $PUBLIC_BRIDGE_MTU
+    sudo ip link set dev $IRONIC_INSPECTOR_INTERFACE mtu $PUBLIC_BRIDGE_MTU
     sudo ip addr add $IRONIC_INSPECTOR_INTERNAL_IP_WITH_NET dev $IRONIC_INSPECTOR_INTERFACE
 
     sudo iptables -I INPUT -i $IRONIC_INSPECTOR_INTERFACE -p udp \
