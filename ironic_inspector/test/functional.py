4f578169c7bb389c6ad930a90cf810487f0a10ea
Dmitry Tantsur <dtantsur@protonmail.com>
2019-07-18 14:25:41 +0200

    Allow running a specific functional test via CLI
    Change-Id: I5897d8121b7cc992d64f948f73f1f846615f6e46

--- ironic_inspector/test/functional.py
+++ ironic_inspector/test/functional.py
@@ -18,6 +18,7 @@ import copy
 import datetime
 import json
 import os
+import sys
 import tempfile
 import time
 import unittest
@@ -798,5 +799,10 @@ def mocked_server():
 
 
 if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        test_name = sys.argv[1]
+    else:
+        test_name = None
+
     with mocked_server():
-        unittest.main(verbosity=2)
+        unittest.main(verbosity=2, defaultTest=test_name)
