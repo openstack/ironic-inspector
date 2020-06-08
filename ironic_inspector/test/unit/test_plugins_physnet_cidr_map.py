80167d4908c23950b84110b7f14d689882423a3e
Hervé Beraud <hberaud@redhat.com>
2020-06-09 01:15:07 +0200

    Use unittest.mock instead of mock
    The mock third party library was needed for mock support in py2
    runtimes. Since we now only support py36 and later, we can use the
    standard lib unittest.mock module instead.
    
    Change-Id: Iccf78a04a66dcef383d9e38ac3990f3c838bdf84

--- ironic_inspector/test/unit/test_plugins_physnet_cidr_map.py
+++ ironic_inspector/test/unit/test_plugins_physnet_cidr_map.py
@@ -11,7 +11,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import mock
+from unittest import mock
+
 from openstack import exceptions
 from oslo_config import cfg
 
